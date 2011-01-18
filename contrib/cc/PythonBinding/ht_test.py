f0c5fb5c7cffeec52b2d0ab6aea990bce021ee7e
Stanislav Yudin <stanislav.yudin@k7cloud.com>
2011-01-18 20:08:56 +0530

    update test with namespace methods: open_namespace, get_namespaces, create_namespace, get_name, get_tables

--- contrib/cc/PythonBinding/ht_test.py
+++ contrib/cc/PythonBinding/ht_test.py
@@ -5,55 +5,58 @@
 #os.system('python setup.py build')
 
 #for path in glob.glob('build/*'):
-#    sys.path.insert(0, path)
+#	 sys.path.insert(0, path)
 
 
 import ht
+schema = '''
+<Schema>
+  <AccessGroup name="default">
+    <ColumnFamily>
+      <Name>app</Name>
+      <deleted>false</deleted>
+    </ColumnFamily>
+    <ColumnFamily>
+      <Name>props</Name>
+      <deleted>false</deleted>
+    </ColumnFamily>
+  </AccessGroup>
+</Schema>
+'''
 
 def test():
-    client = ht.Client("/opt/hypertable/current/conf/hypertable.cfg")
+	client = ht.Client("/opt/hypertable/0.9.4.3.d48b0f4/conf/hypertable.cfg")
+	
+	rootns = client.open_namespace('/')
+	ns_list = rootns.get_namespaces()
+	print 'namespaces:'
+	print repr(ns_list)
+	if not 'testspace' in ns_list:
+		client.create_namespace('testspace', True)
+	ns = client.open_namespace('testspace')
+	print 'tables in %s' % ns.get_name()
+	tbl_list = ns.get_tables()
+	if 'xxx1' in tbl_list:
+		ns.drop_table('xxx1')
+	ns.create_table('xxx1', schema)
+	table = ns.open_table('xxx1')
+	mutator = table.create_mutator()
+	# insert value
+	mutator.set("py-k1", "app", "", "py-v1")
+	mutator.flush()
+	
 
-    print client.hql('show tables')
-    print client.hql('drop table if exists xxx')
-    print client.hql('show tables')
-    print client.hql('create table xxx (yyy)')
-    print client.hql('show tables')
-    print client.get_schema("xxx", True)
+	# insert another value
+	mutator.set("py-k2", "app", "", "py-v2")
+	mutator.flush()
 
-    def selectstar():
-        print ["%s:%s %s" % (cell.row_key, cell.column_family, cell.value) for cell in client.hql('select * from xxx')]
+	# delete value
+	mutator.set_delete("py-k1", "app", "")
+	mutator.flush()
 
-    selectstar()
-
-    print "=== hql examples"
-    print client.hql('insert into xxx values ("666","yyy:777","888")')
-    selectstar()
-
-    print client.hql('delete * from xxx where row="666"')
-    selectstar()
-
-    print "=== mutator examples"
-    table = client.open_table("xxx")
-    mutator = table.create_mutator()
-    # insert value
-    mutator.set("py-k1", "yyy", "", "py-v1")
-    mutator.flush()
-    selectstar()
-
-    # insert another value
-    mutator.set("py-k2", "yyy", "", "py-v2")
-    mutator.flush()
-    selectstar()
-
-    # delete value
-    mutator.set_delete("py-k1", "yyy", "")
-    mutator.flush()
-    selectstar()
-
-    print "=== scanner examples"
-    scan_spec_builder = ht.ScanSpecBuilder()
-    scan_spec_builder.add_row_interval("p", True, "q", False)
-    for cell in table.create_scanner(scan_spec_builder):
-        print "%s:%s %s" % (cell.row_key, cell.column_family, cell.value)
+	scan_spec_builder = ht.ScanSpecBuilder()
+	scan_spec_builder.add_row_interval("p", True, "q", False)
+	for cell in table.create_scanner(scan_spec_builder):
+		print "%s:%s %s" % (cell.row_key, cell.column_family, cell.value)
 
 test()
